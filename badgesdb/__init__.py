a64b046d2462565f68d2c987fa6babeef06b0fd2
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2023-06-20 12:21:14 +0530

    Include runner script and configuration reading
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

---
+++ badgesdb/__init__.py
@@ -0,0 +1,18 @@
+from importlib import metadata
+from logging import getLogger
+from logging.config import dictConfig
+
+from badgesdb.conf import logrdata, standard
+
+__vers__ = metadata.version("badgesdb")
+
+
+def readconf(confobjc):
+    standard.database = confobjc.get("database", standard.database)
+    standard.jsyncurl = confobjc.get("jsyncurl", standard.jsyncurl)
+    standard.asyncurl = confobjc.get("asyncurl", standard.asyncurl)
+    standard.dtbsport = confobjc.get("dtbsport", standard.dtbsport)
+    standard.username = confobjc.get("username", standard.username)
+    standard.password = confobjc.get("password", standard.password)
+    dictConfig(standard.logrconf)
+    logrdata.logrobjc = getLogger(__name__)
---
+++ badgesdb/main.py
@@ -0,0 +1,27 @@
+import click
+
+from badgesdb import __vers__, readconf
+from badgesdb.data.formatdb import make_database
+
+
+@click.group(name="badgesdb")
+@click.option(
+    "-c",
+    "--conffile",
+    "conffile",
+    type=click.Path(exists=True),
+    help="Read configuration from the specified Python file",
+    default=None,
+)
+@click.version_option(version=__vers__, prog_name="badgesdb")
+def main(conffile=None):
+    if conffile:
+        confdict = {}
+        with open(conffile, "r") as confobjc:
+            exec(compile(confobjc.read(), conffile, "exec"), confdict)
+        readconf(confdict)
+
+
+@main.command(name="setupdbs", help="Setup the database schema in the specified environment")
+def setupdbs():
+    make_database()
