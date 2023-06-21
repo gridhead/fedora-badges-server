b18c2391dd8e4adc1634b8f3688a79f2e995d8e1
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2023-06-21 13:59:57 +0530

    Include exception handling for cases when database does not exist
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

--- badgesdb/data/formatdb.py
+++ badgesdb/data/formatdb.py
@@ -13,34 +13,41 @@ migrpath = str(Path(str(Path(__file__).parent.parent), str("migr")))
 
 
 def make_database():
-    engnobjc = make_sync_engine()
-    insprslt = inspect(engnobjc)
-    relation = sorted(indx for indx in metadata.tables if insprslt.has_table(indx))
-
-    if relation:
-        logrdata.logrobjc.error("Relations already available")
-        logrdata.logrobjc.error(", ".join(relation))
-        logrdata.logrobjc.error("Refusing to make changes to the database schema")
+    try:
+        engnobjc = make_sync_engine()
+        insprslt = inspect(engnobjc)
+        relation = sorted(indx for indx in metadata.tables if insprslt.has_table(indx))
+        if relation:
+            logrdata.logrobjc.error("Relations already available")
+            logrdata.logrobjc.error(", ".join(relation))
+            logrdata.logrobjc.error("Refusing to make changes to the database schema")
+            sys.exit(1)
+        with engnobjc.begin():
+            try:
+                logrdata.logrobjc.info("Creating database schema")
+                metadata.create_all(bind=engnobjc)
+                logrdata.logrobjc.info("Setting up database migrations")
+                confobjc = config.Config()
+                confobjc.set_main_option("script_location", migrpath)
+                confobjc.set_main_option(
+                    "sqlalchemy.url",
+                    URL.create(
+                        "postgresql+psycopg2",
+                        username=standard.username,
+                        password=standard.password,
+                        host=standard.jsyncurl,
+                        port=standard.dtbsport,
+                        database=standard.database,
+                    )
+                    .render_as_string(hide_password=False)
+                    .replace("%", "%%"),
+                )
+                command.stamp(confobjc, "head")
+            except Exception as expt:
+                logrdata.logrobjc.error("Could not initialize the database schema")
+                logrdata.logrobjc.error(expt)
+                sys.exit(1)
+    except Exception as expt:
+        logrdata.logrobjc.error("Specified database is not accessible")
+        logrdata.logrobjc.error(expt)
         sys.exit(1)
-
-    with engnobjc.begin():
-        logrdata.logrobjc.info("Creating database schema")
-        metadata.create_all(bind=engnobjc)
-
-        logrdata.logrobjc.info("Setting up database migrations")
-        confobjc = config.Config()
-        confobjc.set_main_option("script_location", migrpath)
-        confobjc.set_main_option(
-            "sqlalchemy.url",
-            URL.create(
-                "postgresql+psycopg2",
-                username=standard.username,
-                password=standard.password,
-                host=standard.jsyncurl,
-                port=standard.dtbsport,
-                database=standard.database,
-            )
-            .render_as_string(hide_password=False)
-            .replace("%", "%%"),
-        )
-        command.stamp(confobjc, "head")
