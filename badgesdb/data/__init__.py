c4cd8ac247e1ed77a979ceba1a8f1350d08a41bf
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2023-06-21 13:59:27 +0530

    Add generation of asynchronous database engine
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

--- badgesdb/data/__init__.py
+++ badgesdb/data/__init__.py
@@ -1,7 +1,10 @@
+import asyncio
 import sys
 
 from sqlalchemy import URL, MetaData, create_engine
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.engine import Engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, create_async_engine
 
 from badgesdb.conf import logrdata, nameconv, standard
 
@@ -25,3 +28,48 @@ def make_sync_engine():
         logrdata.logrobjc.error(f"Cannot create database engine - {expt}")
         sys.exit(1)
     return engnobjc
+
+
+def make_async_engine():
+    engnloca = URL.create(
+        "postgresql+asyncpg",
+        username=standard.username,
+        password=standard.password,
+        host=standard.jsyncurl,
+        port=standard.dtbsport,
+        database=standard.database,
+    )
+    try:
+        engnobjc = create_async_engine(engnloca)
+    except Exception as expt:
+        engnobjc = None
+        logrdata.logrobjc.error(f"Cannot create database engine - {expt}")
+        sys.exit(1)
+    return engnobjc
+
+
+syncsess_generate = sessionmaker(class_=AsyncSession, expire_on_commit=False, future=True)
+asynsess_generate = sessionmaker(expire_on_commit=False, future=True)
+
+
+def init_sync_model(engnobjc: Engine = None):
+    if not engnobjc:
+        syncengn = make_sync_engine()
+        syncsess_generate.configure(bind=syncengn)
+    else:
+        logrdata.logrobjc.error("Could not instantiate a synchronous database session")
+        sys.exit(1)
+
+
+async def init_async_model(engnobjc: AsyncEngine = None):
+    if not engnobjc:
+        asynengn = make_async_engine()
+        asynsess_generate.configure(bind=asynengn)
+    else:
+        logrdata.logrobjc.error("Could not instantiate an asynchronous database session")
+        sys.exit(1)
+
+
+def init_model(syncengn: Engine = None, asyneng: Engine = None):
+    init_sync_model(syncengn)
+    asyncio.run(init_async_model(asyneng))
