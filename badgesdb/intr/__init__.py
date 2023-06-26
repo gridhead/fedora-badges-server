00c178cc745762eca34d99eaa441be8e71316f11
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2023-06-26 17:08:42 +0530

    Add cleanup procedure for the top-level BadgesDB class
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

--- badgesdb/intr/__init__.py
+++ badgesdb/intr/__init__.py
@@ -1,6 +1,8 @@
-# from badgesdb.data import asynsess_generate
-from badgesdb import data
-from badgesdb.data import make_sync_engine
+from sqlalchemy import URL
+from sqlalchemy.ext.asyncio import create_async_engine
+
+from badgesdb.conf import logrdata
+from badgesdb.data import asynsess_generate
 
 
 class BadgesDB:
@@ -10,5 +12,33 @@ class BadgesDB:
         self.jsyncurl = jsyncurl
         self.dtbspost = dtbsport
         self.database = database
-        self.engnobjc = make_sync_engine(username, password, jsyncurl, dtbsport, database)
-        self.sessobjc = data.syncsess_generate.configure(bind=self.engnobjc)  # NEEDS ATTENTION
+        self.engnobjc = create_async_engine(
+            URL.create(
+                drivername="postgresql+asyncpg",
+                username=self.username,
+                password=self.password,
+                host=self.jsyncurl,
+                port=self.dtbspost,
+                database=self.database,
+            )
+        )
+        sesclass = asynsess_generate
+        sesclass.configure(bind=self.engnobjc)
+        self.sessobjc = sesclass()
+
+    async def cleanall(self):
+        try:
+            # yield self.sessobjc
+            # I DO NOT NEED A LONG-LIVING SESSION OBJECT SO I CHOSE AGAINST "YIELDING" THE SESSOBJC
+            # @nilsph, CORRECT ME IF I AM MISTAKEN.
+            # Reference was taken from
+            # https://github.com/CentOS/duffy/blob/dev/duffy/app/database.py
+            await self.sessobjc.commit()
+        except Exception as expt:
+            await self.sessobjc.rollback()
+            logrdata.logrobjc.error(
+                f"Could not perform the requested transaction on the database - {expt}"
+            )
+            raise
+        finally:
+            await self.sessobjc.close()
