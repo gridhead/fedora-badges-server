5c7dcbcee4f31f382318b6826b37f1f68532a505
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2023-06-26 17:10:41 +0530

    Enable instantiation of the BadgesDB class with active inheritance
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

--- badgesdb/intr/user.py
+++ badgesdb/intr/user.py
@@ -1,23 +1,37 @@
 from sqlalchemy import select
 from sqlalchemy.orm import selectinload
 
+from badgesdb.conf import logrdata
 from badgesdb.form.user import UserPeruseSole_Parameter
 from badgesdb.intr import BadgesDB
 from badgesdb.objs.user import User
 
 
-class IntrUser:
-    def __init__(self, badgesdb: BadgesDB):
-        super().__init__()
-        self.badgesdb = badgesdb
+class IntrUser(BadgesDB):
+    def __init__(self, username, password, jsyncurl, dtbsport, database):
+        super().__init__(
+            username=username,
+            password=password,
+            jsyncurl=jsyncurl,
+            dtbsport=dtbsport,
+            database=database,
+        )
+        self.relaname = "USER"
 
     def create(self):
         pass
 
-    def peruse_sole(self, objciden: UserPeruseSole_Parameter):
-        quryobjc = select(User).filter_by(id=objciden.id).options(selectinload("*"))
-        rsltobjc = self.badgesdb.sessobjc.execute(quryobjc)
-        return rsltobjc.scalar_one_or_none()
+    async def peruse_sole(self, objciden: UserPeruseSole_Parameter):
+        try:
+            quryobjc = select(User).filter_by(id=objciden.id).options(selectinload("*"))
+            rsltobjc = await self.sessobjc.execute(quryobjc)
+            await self.cleanall()
+            return rsltobjc.scalar_one_or_none()
+        except Exception as expt:
+            logrdata.logrobjc.error(
+                f"Could not read one record from the relation {self.relaname} - {expt}"
+            )
+            return False
 
     def peruse_many(self):
         pass
