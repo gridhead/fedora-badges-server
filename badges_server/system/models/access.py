9f791b9a837e19976a910f80a18a9d76c154b430
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2023-07-20 10:47:20 +0530

    Add endpoint for refreshing the access code
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

---
+++ badges_server/system/models/access.py
@@ -0,0 +1,34 @@
+from abc import ABC
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+
+from badges_server.system.common import APIResult
+
+
+class AccessBase(BaseModel, ABC):
+    """
+    Base: User
+    """
+
+    class Config:
+        orm_mode = True
+
+    id: Optional[int]
+    user_id: Optional[int]
+    stopdate: Optional[datetime]
+    code: Optional[str]
+    active: Optional[bool]
+    makedate: Optional[datetime]
+    uuid: Optional[str]
+
+
+class AccessModel(AccessBase):
+    code: str
+    makedate: datetime
+    uuid: str
+
+
+class AccessResult(APIResult):
+    access: AccessModel
