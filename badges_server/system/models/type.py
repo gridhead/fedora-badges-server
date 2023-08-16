6b1905b7b39749182ffc65d03542743d8127f372
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2023-08-16 09:46:57 +0530

    Add endpoints for adding accolade type
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

---
+++ badges_server/system/models/type.py
@@ -0,0 +1,43 @@
+from abc import ABC
+from typing import Optional
+
+from pydantic import BaseModel
+
+from badges_server.system.common import APIResult
+
+
+class TypeBase(BaseModel, ABC):
+    """
+    Base: Type
+    """
+
+    class Config:
+        orm_mode = True
+
+
+class TypeModelExternal(TypeBase):
+    name: Optional[str]
+    desc: Optional[str]
+    arranged: Optional[bool]
+    uuid: Optional[str]
+
+
+class TypeModelInternal(TypeBase):
+    id: Optional[int]
+
+
+class TypeModel(TypeBase):
+    name: str
+    desc: str
+    arranged: bool
+    uuid: str
+
+
+class TypeCreateModel(BaseModel):
+    name: str
+    desc: Optional[str]
+    arranged: bool = False
+
+
+class TypeResult(APIResult):
+    type: TypeModel
